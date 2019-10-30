e3a0c9819cd8549df71aa975909c97c1f9d1e829
Punchaya Siriphanthong <p.siriphanthong@gmail.com>
2019-10-31 02:45:52 +0700

    setup styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 import logo from './logo.svg'
-import './App.css'
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
+const Wrapper = styled.div`
+  text-align: center;
+`
+
+const Header = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Logo = styled.img.attrs({
+  src: logo,
+  alt: 'logo',
+})`
+  height: 40vmin;
+`
+
+const Link = styled.a`
+  color: #09d3ac;
+`
+
+const App = () => (
+  <Wrapper>
+    <Header>
+      <Logo />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Link
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </Link>
+    </Header>
+  </Wrapper>
+)
 
 export default App
