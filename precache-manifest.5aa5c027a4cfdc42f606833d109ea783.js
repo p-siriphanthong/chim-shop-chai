9a0b028db3bddeb8543f6c7004acec20d12ab429
Punchaya Siriphanthong <p.siriphanthong@gmail.com>
2019-11-02 02:54:04 +0700

    Updates

---
+++ precache-manifest.5aa5c027a4cfdc42f606833d109ea783.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52216db7b23c45ed730263b2b9f2bbe0",
+    "url": "/chim-shop-chai/index.html"
+  },
+  {
+    "revision": "0337f5bbdfbc907ca2a0",
+    "url": "/chim-shop-chai/static/js/2.c0d28129.chunk.js"
+  },
+  {
+    "revision": "8bdb22d8bcc35fbb81ce",
+    "url": "/chim-shop-chai/static/js/main.330276cd.chunk.js"
+  },
+  {
+    "revision": "2e252453f70dbe8d756f",
+    "url": "/chim-shop-chai/static/js/runtime-main.3037b025.js"
+  }
+]);
